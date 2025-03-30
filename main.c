4cc40a110420af08a0e07dcccd22236ae1bbb181
donthurtme1 <malwarecunt@proton.me>
2025-03-30 23:56:51 +0100

    Changed name of main c file to main.c

--- main.c
+++ main.c
@@ -11,8 +11,10 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
+#include <poll.h>
+#include <sys/socket.h>
 
-#include "util.h"
+#define VOLUME
 
 void cleanup(void);
 void sigint(int signum);
@@ -23,27 +25,25 @@ static const char *mdaypostfix[] = { "st", "nd", "rd" };
 static const char *monstr[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
 static char status[64];
-static pid_t dwlb_pid, wireplumber_pid;
+static pid_t dwlb_pid;
 
 void
 cleanup(void) {
 	if (dwlb_pid) kill(dwlb_pid, SIGTERM);
-	if (wireplumber_pid) kill(wireplumber_pid, SIGTERM);
 }
 
 void
-sigint(int signum) {
-	if (dwlb_pid) kill(dwlb_pid, SIGINT);
-	if (wireplumber_pid) kill(wireplumber_pid, SIGINT);
-	putc('\n', stdout);
-	exit(EXIT_SUCCESS);
-}
-
-void
-sigkill(int signum) {
-	if (dwlb_pid) kill(dwlb_pid, SIGKILL);
-	if (wireplumber_pid) kill(wireplumber_pid, SIGKILL);
-	exit(EXIT_SUCCESS);
+sighandler(int signum) {
+	switch (signum) {
+		case SIGINT:
+			if (dwlb_pid) kill(dwlb_pid, SIGINT);
+			exit(EXIT_SUCCESS);
+			break;
+		case SIGKILL:
+			if (dwlb_pid) kill(dwlb_pid, SIGKILL);
+			exit(EXIT_SUCCESS);
+			break;
+	}
 }
 
 int
@@ -51,14 +51,14 @@ main(int argc, char *argv[]) {
 	static struct termios term;
 	char timestr[6] = "00:00";
 	const char *s;
-	float volume;
+	char *volume;
 	time_t timer;
 	struct tm tm;
 	struct timespec tc;
 
 	/* Setup */
-	signal(SIGINT, &sigint);
-	signal(SIGKILL, &sigkill);
+	signal(SIGINT, &sighandler);
+	signal(SIGKILL, &sighandler);
 	atexit(&cleanup);
 
 	/* Setup pipes */
@@ -77,6 +77,10 @@ main(int argc, char *argv[]) {
 	close(dwlb_fd[0]);
 	dup2(dwlb_fd[1], STDOUT_FILENO);
 
+#ifdef VOLUME
+	/* Open pipewire socket */
+#endif
+
 #ifdef LAPTOP
 	/* Setup power supply file pointers */
 	FILE *bat_now_fp, *bat_full_fp;
@@ -91,26 +95,30 @@ main(int argc, char *argv[]) {
 #endif
 
 	while (1) {
-		if (clock_gettime(CLOCK_MONOTONIC, &tc) == -1)
-			die("clock_gettime");
+		clock_gettime(CLOCK_MONOTONIC, &tc);
 		time(&timer);
-		if (localtime_r(&timer, &tm) == NULL)
-			die("localtime_r");
+		localtime_r(&timer, &tm);
 
+#ifdef VOLUME
 		/* Volume */
-		//int wireplumber_fd[2];
-		//pipe(wireplumber_fd);
-		//wireplumber_pid = fork();
-		//if (wireplumber_pid == 0) {
-		//	close(wireplumber_fd[0]);
-		//	dup2(wireplumber_fd[1], STDOUT_FILENO);
-		//	execvp("wpctl", (char *[]){ "wpctl", "get-volume", "@DEFAULT_AUDIO_SINK@", NULL });
-		//	exit(EXIT_FAILURE);
-		//}
-		//dup2(wireplumber_fd[0], STDIN_FILENO);
-		//close(wireplumber_fd[1]);
-		//wait(&wireplumber_pid);
-		//fscanf(stdin, "%*s%f\n", &volume);
+		int pulse_fd[2];
+		pipe(pulse_fd);
+		int pulse_pid = fork();
+		if (pulse_pid == 0) {
+			close(pulse_fd[0]);
+			dup2(pulse_fd[1], STDOUT_FILENO);
+			execvp("pulsemixer", (char *[]){ "pulsemixer", "--get-volume", "--id", "60", NULL });
+			exit(EXIT_FAILURE);
+		}
+		dup2(pulse_fd[0], STDIN_FILENO);
+		close(pulse_fd[1]);
+		size_t sizezezeze;
+		if (wait(&pulse_pid) < 0)
+			kill(pulse_pid, SIGKILL);
+		int len = getline(&volume, &sizezezeze, stdin);
+		volume[len - 1] = '\0';
+		close(pulse_fd[0]);
+#endif
 
 #ifdef LAPTOP
 		/* Laptop charge */
@@ -120,7 +128,8 @@ main(int argc, char *argv[]) {
 
 		/* Only do calculations if the time has changed */
 		static int old_min = -1;
-		if (old_min != tm.tm_min) {
+		static float old_volume = -1;
+		if (1) {
 			old_min = tm.tm_min;
 
 			/* Clock string */
@@ -138,8 +147,8 @@ main(int argc, char *argv[]) {
 #ifdef LAPTOP
 			printf("Bat %d%%   --    ", (int)(((float)now / (float)full) * 100));
 #endif
-#ifdef VOL
-			printf("Vol %d%%   --    ", (int)(volume * 100));
+#ifdef VOLUME
+			printf("Vol %s%%   --    ", volume);
 #endif
 			printf("%s %d%s %s %d  ::  %s (UTC+%d)\n", wdaystr[tm.tm_wday], tm.tm_mday, s, monstr[tm.tm_mon], tm.tm_year + 1900, timestr, (int)(tm.tm_gmtoff / 3600));
 			fflush(stdout);
@@ -147,8 +156,8 @@ main(int argc, char *argv[]) {
 
 		tc.tv_sec++;
 		tc.tv_nsec = 0;
-		if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &tc, NULL) != 0)
-			die("clock_nanosleep");
+		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &tc, NULL);
+
 	}
 
 	return 0;
